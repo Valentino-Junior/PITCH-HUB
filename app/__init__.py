0a208224d0a9ef3b7d565a5df0a40365cf1f41c2
valentine-ochieng <valentine.ochieng@student.moringaschool.com>
2021-11-08 10:21:52 +0300

    Installed flask

---
+++ app/__init__.py
@@ -0,0 +1,14 @@
+from flask_sqlalchemy import SQLAlchemy
+
+bootstrap = Bootstrap()
+db = SQLAlchemy()
+
+def create_app(config_name):
+    app = Flask(__name__)
+
+
+    # Initializing flask extensions
+    bootstrap.init_app(app)
+    db.init_app(app)
+
+    
