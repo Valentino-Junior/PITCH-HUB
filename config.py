f33f459e58d7227db1f91efd07fa9130479cd131
valentine-ochieng <valentine.ochieng@student.moringaschool.com>
2021-11-09 08:48:26 +0300

    Added sqlalcjemy database uri

--- config.py
+++ config.py
@@ -1,2 +1,52 @@
+import os
+
 class Config:
-     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://username:password@localhost/watchlist'
+    '''
+    General configuration parent class
+    '''
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:valentino:+254@localhost/pitch'
+    UPLOADED_PHOTOS_DEST ='app/static'
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+    # email configurations
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+class TestConfig(Config):
+    '''
+    Testing configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:rock1ville@localhost/minutepitch'
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:rock1ville@localhost/minutepitch'
+
+    DEBUG = True
+    ENV = 'development'
+    
+config_options = {
+'development':DevConfig,
+'production':ProdConfig,
+'test':TestConfig
+}
